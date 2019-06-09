29a60ed1fc14800d163fd7d110ea0ec8ebd59993
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-09 21:53:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,9 @@
-def roll_call_dwarves(dwarves)# code an argument here
+def roll_call_dwarves# code an argument here
   # Your code here
-
-  dwarves.collect do |dwarves|
-    puts dwarves
-  end
+  puts array
 end
 
-roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
-
-def summon_captain_planet(planeteer_calls)# code an argument here
+def summon_captain_planet# code an argument here
   # Your code here
 end
 
@@ -16,12 +11,7 @@ def long_planeteer_calls# code an argument here
   # Your code here
 end
 
-def find_the_cheese(array)# code an argument here
+def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-
-  array.include?(cheese_types)
-
 end
-
-find_the_cheese(["tomato soup", "cheddar", "oyster crackers", "gouda"])
