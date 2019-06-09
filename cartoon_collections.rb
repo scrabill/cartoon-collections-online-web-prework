d252dbabc0f71b9faf8bd352d1a2d51f2505d0c1
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-09 22:32:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,6 +28,7 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array = []
 
   # for each item in the array
   #   see if one of the cheese types is include
