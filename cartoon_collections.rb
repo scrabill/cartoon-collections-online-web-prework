f4de5e83f9900bd1a5cfdd8e667e2b295d494e19
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-09 21:56:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,7 @@ def roll_call_dwarves(array)# code an argument here
   # Your code here
   array.each.with_index do |index,name|
     puts "#{index}. #{name}"
+  end
 end
 
 def summon_captain_planet# code an argument here
