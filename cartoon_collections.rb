effdb0bb657161dccf1ec3b34217e29beb88de94
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-09 22:12:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def summon_captain_planet(array)# code an argument here
   # planeteer_calls[0].slice(1, planeteer_calls.length-1) >> returns everything but the first elements in the array element
   # planeteer_calls[0][0].upcase >> make just the first letter uppercase
   array.collect do |x,index|
-    x
+    x[0].upcase + x.slice(1, x.length-1)
     x + "!"
   end
 end
